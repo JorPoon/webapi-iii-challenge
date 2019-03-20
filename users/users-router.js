0c348da6e8d087f564fec23cd5bc49663683cdc0
Jor_Poon <jormpoon@gmail.com>
2019-03-20 17:58:31 -0400

    able to add new user

--- users/users-router.js
+++ users/users-router.js
@@ -7,8 +7,8 @@ const router = express.Router();
 // this only runs if the url has /api/users in it
 router.get('/', async (req, res) => {
     try {
-        const users = await Users.get(req)
-        res.status(200).json(users);
+        const user = await Users.get(req)
+        res.status(200).json(user);
     } catch (error) {
         res.status(500).json({
             message: 'Error retreiving users'
@@ -22,6 +22,34 @@ router.get('/', async (req, res) => {
     // .catch(error => {
     //     res.status(500).json({error: 'The user information cannot be retrieved'})
     // });
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.getById(req.params.id);
+
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({message: 'User not found'});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: 'Error retrieving user'
+        })
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const user = await Users.insert(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({
+            Error: 'Error adding the user'
+        })
+    }
 })
 
 module.exports = router;
